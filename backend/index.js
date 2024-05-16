a6b53fd34a1a1d0eceff99b9f38b08961ec90d92
Rishabh Jain <107530140+rishabhjain208@users.noreply.github.com>
2024-05-16 23:40:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 const corsOptions = {
-  origin: [""],
+  origin: ["https://medicare-bookings.vercel.app"],
   methods: ['POST', 'GET', 'PUT', 'DELETE'],
   credentials: true,
 };
