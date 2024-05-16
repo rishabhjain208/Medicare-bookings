7a3e42064bd9dcf89f943e0e7f5c5819d5ffa1af
Rishabh Jain <107530140+rishabhjain208@users.noreply.github.com>
2024-05-16 21:34:05 +0530

    final code

---
+++ frontend/src/config.js
@@ -0,0 +1,3 @@
+export const BASE_URL = "http://localhost:5000/api/v1";
+// export const BASE_URL = "https://medicare-booking-2-at50.onrender.com";
+export const token = localStorage.getItem("token");
