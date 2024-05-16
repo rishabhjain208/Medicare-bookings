5bbaaac2dbdd5404093205fce0bb38cd188bab26
Rishabh Jain <107530140+rishabhjain208@users.noreply.github.com>
2024-05-16 22:30:55 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
-// export const BASE_URL = "https://medicare-bookings-api.vercel.app/";
+// export const BASE_URL = "http://localhost:5000/api/v1";
+export const BASE_URL = "https://medicare-bookings-8gm9hcvwa-rishabh-jains-projects-25bcb276.vercel.app/";
 // export const BASE_URL = "https://medicare-booking-2-at50.onrender.com";
 export const token = localStorage.getItem("token");
